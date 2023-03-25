4273dfe094e976e1a170d2465066ad355c178118
wildanmf13 <wildanmfauzan85@gmail.com>
2023-03-25 11:19:51 +0700

    odingan Project

---
+++ PHP/index.php
@@ -0,0 +1,59 @@
+<?php
+include("koneksi.php");
+// query untuk menampilkan data
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Data Barang</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Data Barang</h1>
+        <div class="main">
+            <table border="3">
+                <tr>
+                    <th>Gambar</th>
+                    <th>Nama Barang</th>
+                    <th>Kategori</th>
+                    <th>Harga Jual</th>
+                    <th>Harga Beli</th>
+                    <th>Stok</th>
+                    <th>Aksi</th>
+                    <th>Ubah</th>
+                    <th>Tambah</th>
+                    <th>Hapus</th>
+                </tr>
+                <?php if ($result) : ?>
+                    <?php while ($row = mysqli_fetch_array($result)) : ?>
+                        <tr>
+
+                            <td><img src="gambar/<?= $row['gambar']; ?>" alt="<?= $row['nama']; ?>"></td>
+                            <td><?= $row['nama']; ?></td>
+                            <td><?= $row['kategori']; ?></td>
+                            <td><?= $row['harga_beli']; ?></td>
+                            <td><?= $row['harga_jual']; ?></td>
+                            <td><?= $row['stok']; ?></td>
+                            <td><?= $row['id_barang']; ?></td>
+                            <td><a href="ubah.php?id=<?= $row['id_barang']; ?>">Ubah</a></td>
+                            <td><a href="tambah.php?id=<?= $row['id_barang']; ?>">Tambah</a></td>
+                            <td><a href="hapus.php?id=<?= $row['id_barang']; ?>">Hapus</a></td>
+                        </tr>
+                    <?php endwhile;
+                else : ?>
+                    <tr>
+                        <td colspan="7">Belum ada data</td>
+                    </tr>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
